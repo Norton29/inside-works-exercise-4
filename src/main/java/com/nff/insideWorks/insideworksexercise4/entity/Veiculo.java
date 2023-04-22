ddd4494b785a9ddb880725ea1b50180d89844e34
Norton <norton290194@gmail.com>
2023-04-22 20:48:59 -0300

    Ajuste

--- src/main/java/com/nff/insideWorks/insideworksexercise4/entity/Veiculo.java
+++ src/main/java/com/nff/insideWorks/insideworksexercise4/entity/Veiculo.java
@@ -1,16 +1,21 @@
 package com.nff.insideWorks.insideworksexercise4.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
 @AllArgsConstructor
+@Table(name = "veiculo", uniqueConstraints = @UniqueConstraint(columnNames = {"chassi", "cidade"}))
 public class Veiculo {
 	
+	@Id
 	private String placa;
 	private Integer chassi;
 	private String cidade;
